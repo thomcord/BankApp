608d14fcc2c1b3f9330b9bcf74ae9e7e652940c2
thomcord <thomcord@gmail.com>
2021-08-18 08:49:24 +0200

    first commit - started app

---
+++ Bank/src/bank/Account.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Account {
+	//Variables
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	//Constructor
+	Account (String cname, String cid) {
+		
+	}
+}
---
+++ Bank/src/bank/main.java
@@ -0,0 +1,13 @@
+package bank;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Account thomas = new Account("Thomas Cordeiro", "A0001");
+		thomas.showMenu();
+
+	}
+
+}
